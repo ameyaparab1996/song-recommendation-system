7566a8d0c938ce7beb16f3bf8e045265a804b0b3
ameyaparab1996 <98881388+ameyaparab1996@users.noreply.github.com>
2023-11-23 01:12:15 -0500

    Update song_recommendation_app.py

--- song_recommendation_app.py
+++ song_recommendation_app.py
@@ -6,7 +6,7 @@ def intro():
     page_bg_img = '''
     <style>
     body {
-    background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
+    background-image: url("https://unsplash.com/photos/black-remote-control-on-white-textile-6k4HkET8dPM");
     background-size: cover;
     }
     </style>
