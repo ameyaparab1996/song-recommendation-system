5de6435cd0a04c18c29618c5276e809fe86366e6
ameyaparab1996 <98881388+ameyaparab1996@users.noreply.github.com>
2023-11-23 12:32:44 -0500

    Update song_recommendation_app.py

--- song_recommendation_app.py
+++ song_recommendation_app.py
@@ -144,7 +144,7 @@ def generate_recommendations(positive_prompt, negative_prompt, n):
 
     st.markdown("""
         <style>
-            .stDataFrame tr {
+            table tr {
                 height: 50px;
             }
         </style>
