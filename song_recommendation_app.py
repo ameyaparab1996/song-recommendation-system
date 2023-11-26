94add67bd9bc6711956a8f15e30c4fd1b31d171a
ameyaparab1996 <98881388+ameyaparab1996@users.noreply.github.com>
2023-11-26 01:13:07 -0500

    Update song_recommendation_app.py

--- song_recommendation_app.py
+++ song_recommendation_app.py
@@ -166,17 +166,18 @@ def display_recommendations(spotify_df):
     include = [True] * (len(spotify_df))
     spotify_df['include'] = [True] * (len(spotify_df))
     
-    def update_include(value):
+    def update_include():
         spotify_df['include'] = include
-        spotify_df.loc[6, 'include'] = value
         st.session_state.checkbox = True
 
-    for j in range(0, len(spotify_df)):
-        album_image_col.image(spotify_df.iloc[j, 4], caption=spotify_df.iloc[j, 2])
-        track_name_col.markdown('<p>' + spotify_df.iloc[j, 1] + '</p>', unsafe_allow_html=True)
-        artists_col.markdown('<p>' + ', '.join(spotify_df.iloc[j, 3]) + '</p>', unsafe_allow_html=True)
-        preview_col.audio(spotify_df.iloc[j, 5], format="audio/mp3")
-        include[j] = playlist_col.checkbox("",key=j, value=spotify_df.iloc[j, 6], label_visibility="collapsed", on_change=update_include(value))
+    with st.form(key='my_form'):
+        for j in range(0, len(spotify_df)):
+            album_image_col.image(spotify_df.iloc[j, 4], caption=spotify_df.iloc[j, 2])
+            track_name_col.markdown('<p>' + spotify_df.iloc[j, 1] + '</p>', unsafe_allow_html=True)
+            artists_col.markdown('<p>' + ', '.join(spotify_df.iloc[j, 3]) + '</p>', unsafe_allow_html=True)
+            preview_col.audio(spotify_df.iloc[j, 5], format="audio/mp3")
+            include[j] = playlist_col.checkbox("",key=j, value=spotify_df.iloc[j, 6], label_visibility="collapsed")
+        st.form_submit_button(label='Submit', on_click=update_include())
     #st.stop()
 
     ##### Option using a container #####
