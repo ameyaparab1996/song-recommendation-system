1cf34683e0768a1c81f85adf9b95584f793bb266
ameyaparab1996 <98881388+ameyaparab1996@users.noreply.github.com>
2023-11-25 22:09:02 -0500

    Update song_recommendation_app.py

--- song_recommendation_app.py
+++ song_recommendation_app.py
@@ -146,7 +146,7 @@ def display_recommendations(spotify_df):
 
         h3 {
             text-align: center !important;
-            height: 50px !important;
+            height: 100px !important;
         } 
         
         p {
@@ -163,7 +163,7 @@ def display_recommendations(spotify_df):
     preview_col.subheader("Preview       ", divider='green')
     playlist_col.subheader("Add to Playlist", divider='green')
     
-    include = []
+    include = [True] * len(spotify_df)
     def update_include():
         spotify_df['include'] = include
     
