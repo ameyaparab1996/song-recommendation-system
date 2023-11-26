2f0b1b156d10004bf7d9c2c5048dcfcc878ef038
ameyaparab1996 <98881388+ameyaparab1996@users.noreply.github.com>
2023-11-26 00:26:23 -0500

    Update song_recommendation_app.py

--- song_recommendation_app.py
+++ song_recommendation_app.py
@@ -163,11 +163,10 @@ def display_recommendations(spotify_df):
     preview_col.subheader("Preview       ", divider='green')
     playlist_col.subheader("Add to Playlist", divider='green')
     
-    include = [True] * len(spotify_df)
+    include = [True] * (len(spotify_df)-1)
     
     def update_include():
         spotify_df['include'] = include
-        print(include)
         st.session_state.checkbox = True
     
     for j in range(0, len(spotify_df)):
@@ -176,7 +175,7 @@ def display_recommendations(spotify_df):
         track_name_col.markdown('<p>' + spotify_df.iloc[j, 1] + '</p>', unsafe_allow_html=True)
         artists_col.markdown('<p>' + ', '.join(spotify_df.iloc[j, 3]) + '</p>', unsafe_allow_html=True)
         preview_col.audio(spotify_df.iloc[j, 5], format="audio/mp3")
-        include[j] = playlist_col.checkbox("",key=j,value=True, on_change=update_include())
+        include[j] = playlist_col.checkbox("",key=j, on_change=update_include())
     #st.stop()
 
     ##### Option using a container #####
