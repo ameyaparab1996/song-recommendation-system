e25c90eab7465deac22e7876eba621a53e9fe7fc
ameyaparab1996 <98881388+ameyaparab1996@users.noreply.github.com>
2023-11-26 23:26:10 -0500

    Update song_recommendation_app.py

--- song_recommendation_app.py
+++ song_recommendation_app.py
@@ -220,8 +220,6 @@ def display_recommendations(spotify_df, positive_prompt):
             preview_col.audio(spotify_df.iloc[j, 5], format="audio/mp3")
             include[j] = playlist_col.checkbox("",key=j, value=spotify_df.iloc[j, 7], label_visibility="collapsed")
         username = st.text_input('Spotify Username', help="To find your username go to Settings and privacy > Account")
-        if username != "":
-            spotify_login()
         playlist_name = st.text_input('Playlist Name', help="Give a name to your playlist which will appear in your library")
         create_button = st.form_submit_button(label='Create Playlist')
         if create_button:
