efe5e4387b287bb3509a3ff5b7656d66b87268df
ameyaparab1996 <98881388+ameyaparab1996@users.noreply.github.com>
2023-11-27 18:50:10 -0500

    Update song_recommendation_app.py

--- song_recommendation_app.py
+++ song_recommendation_app.py
@@ -259,7 +259,7 @@ def display_recommendations(spotify_df, positive_prompt):
                 artists_col.markdown('<p>' + ', '.join(spotify_df.iloc[j, 3]) + '</p>', unsafe_allow_html=True)
                 preview_col.audio(spotify_df.iloc[j, 5], format="audio/mp3")
                 logger.info("before checkbox" + str(st.session_state.create))
-                include[j] = playlist_col.checkbox("",key=j, value=spotify_df.iloc[j, 7], label_visibility="collapsed")
+                include[j] = playlist_col.checkbox("",key=j, value=spotify_df.iloc[j, 7], label_visibility="collapsed", on_change= update_include())
 
             st.session_state.username = st.text_input('Spotify Username' ,help="To find your username go to Settings and privacy > Account")
             auth_url = st.session_state.sp_oauth.get_authorize_url()
