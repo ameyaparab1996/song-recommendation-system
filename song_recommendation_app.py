3b0fbe801c6d41348457adf2380e424f9b4c2703
ameyaparab1996 <98881388+ameyaparab1996@users.noreply.github.com>
2023-11-27 02:55:11 -0500

    Update song_recommendation_app.py

--- song_recommendation_app.py
+++ song_recommendation_app.py
@@ -169,7 +169,7 @@ def generate_recommendations(positive_prompt, negative_prompt, n):
         display_recommendations(st.session_state.spotify_df, positive_prompt)
 
     def spotify_redirect(sp_oauth, redirected_url, track_uri, username, playlist_name, playlist_description):
-        logger.info("inside spotify redirect" + st.session_state.redirect)
+        logger.info("inside spotify redirect")
         logger.info(st.session_state.username + st.session_state.playlist_name)
         parsed_url = urlparse(redirected_url)
         query_params = parse_qs(parsed_url.query)
@@ -262,11 +262,11 @@ def display_recommendations(spotify_df, positive_prompt):
                 artists_col.markdown('<p>' + ', '.join(spotify_df.iloc[j, 3]) + '</p>', unsafe_allow_html=True)
                 preview_col.audio(spotify_df.iloc[j, 5], format="audio/mp3")
                 include[j] = playlist_col.checkbox("",key=j, value=spotify_df.iloc[j, 7], label_visibility="collapsed")
-            username = st.text_input('Spotify Username', value = st.session_state.username ,help="To find your username go to Settings and privacy > Account")
+            username = st.text_input('Spotify Username' ,help="To find your username go to Settings and privacy > Account")
             auth_url = st.session_state.sp_oauth.get_authorize_url()
             st.markdown(f"[Login with Spotify]({auth_url})")
             redirected_url = st.text_input("Enter the redirected URL after login:")
-            playlist_name = st.text_input('Playlist Name', value = st.session_state.playlist_name, help="Give a name to your playlist which will appear in your library")
+            playlist_name = st.text_input('Playlist Name', help="Give a name to your playlist which will appear in your library")
             logger.info("before submit" + str(st.session_state.create))
             create_button = st.form_submit_button(label='Create Playlist', on_click=before_submit())
             if create_button:
