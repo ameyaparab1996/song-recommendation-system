23344de3064c2df4596ed7c5dc4158005a8db169
ameyaparab1996 <98881388+ameyaparab1996@users.noreply.github.com>
2023-11-29 18:16:03 -0500

    Update song_recommendation_app.py

--- song_recommendation_app.py
+++ song_recommendation_app.py
@@ -258,7 +258,7 @@ def display_recommendations(spotify_df, positive_prompt):
             preview_col.audio(spotify_df.iloc[j, 5], format="audio/mp3")
             logger.info("before checkbox" + str(st.session_state.create))
             st.session_state.include[j] = playlist_col.checkbox("",key=j, value=spotify_df.iloc[j, 7], label_visibility="collapsed")
-            if st.session_state.include[j] != spotify_df.iloc[j, 7]
+            if st.session_state.include[j] != spotify_df.iloc[j, 7]:
                 update_include()
                 logger.info(f"Updated index", j)
 
