083138a8b60453525e11ee8e51bb04712970cb16
ameyaparab1996 <98881388+ameyaparab1996@users.noreply.github.com>
2023-12-01 19:59:04 -0500

    Update song_recommendation_app.py

--- song_recommendation_app.py
+++ song_recommendation_app.py
@@ -188,7 +188,7 @@ def set_background(main_bg):
          <style>
          .stApp {{
              background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()});
-             background-size: auto
+             background-size: contain
          }}
          </style>
          """,
